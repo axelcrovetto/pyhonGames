b7a10a3258fcc17347fbdfff797ac9f19725ce22
axelcrovetto <axelcrovetto2102@gmail.com>
2022-09-17 20:37:27 +0200

    add scores

--- flappy_bird.py
+++ flappy_bird.py
@@ -7,26 +7,27 @@ pygame.init()
 sfondo = pygame.image.load(
     'C:/Users/axelc/Desktop/GiochiPython/immagini/sfondo.png')
 uccello = pygame.image.load(
-    'C:/Users/axelc/Desktop/GiochiPython/immagini/chiccox2.jpg')
+    'C:/Users/axelc/Desktop/GiochiPython/immagini/uccello.png')
 base = pygame.image.load(
     'C:/Users/axelc/Desktop/GiochiPython/immagini/base.png')
 gameover = pygame.image.load(
     'C:/Users/axelc/Desktop/GiochiPython/immagini/gameover.png')
 tubo_giu = pygame.image.load(
-    'C:/Users/axelc/Desktop/GiochiPython/immagini/bottle.png')
+    'C:/Users/axelc/Desktop/GiochiPython/immagini/tubo.png')
 tubo_su = pygame.transform.flip(tubo_giu, False, True)
 
 # aggiungo la finestra di gioco
 
 SCHERMO = pygame.display.set_mode((288, 512))
 FPS = 50
-VEL_AVANZ = 3 
+VEL_AVANZ = 3
+FONT = pygame.font.SysFont('Comic Sans Ms', 50, bold=True)
 
 
 class tubi_classe:
     def __init__(self):
         self.x = 300
-        self.y = random.randint(-75+15, 150+30)
+        self.y = random.randint(-75, 150)
 
     def avanza_e_disegna(self):
         self.x -= VEL_AVANZ
@@ -34,7 +35,7 @@ class tubi_classe:
         SCHERMO.blit(tubo_su, (self.x, self.y-210))
 
     def collissione(self, uccello, uccellox, uccelloy):
-        tolleranza = 10
+        tolleranza = 5
         uccello_lato_dx = uccellox+uccello.get_width() - tolleranza
         uccello_lato_sx = uccellox+tolleranza
         tubi_lato_dx = self.x+tubo_giu.get_width()
@@ -47,6 +48,15 @@ class tubi_classe:
         if uccello_lato_dx> tubi_lato_sx and uccello_lato_sx<tubi_lato_dx :
             if uccello_lato_su<tubi_lato_su or uccello_lato_giu > tubi_lato_giu :
                 hai_perso()
+    def fra_i_tubi(self,ucello,ucellox):
+        tolleranza= 5
+        uccello_lato_dx = uccellox+uccello.get_width() - tolleranza
+        uccello_lato_sx = uccellox+tolleranza
+        tubi_lato_dx = self.x+tubo_giu.get_width()
+        tubi_lato_sx = self.x
+        if uccello_lato_dx > tubi_lato_sx and uccello_lato_sx < tubi_lato_dx:
+            return True
+        
 
 
 def disegna_oggetti():
@@ -56,7 +66,8 @@ def disegna_oggetti():
 
     SCHERMO.blit(uccello, (uccellox, uccelloy))
     SCHERMO.blit(base, (basex, 400))
-
+    punti_render= FONT.render(str(punti),1,(255,255,255))
+    SCHERMO.blit(punti_render,(144,0))
 
 def aggiorna():
     pygame.display.update()
@@ -67,12 +78,15 @@ def inizializza():
     global uccellox, uccelloy, uccello_vely
     global basex
     global tubi
+    global punti
+    global fra_i_tubi
     uccellox, uccelloy = 60, 150
     uccello_vely = 0
     basex = 0
     tubi = []
     tubi.append(tubi_classe())
-
+    punti = 0
+    fra_i_tubi = False
 
 def hai_perso():
     SCHERMO.blit(gameover, (50, 180))
@@ -95,7 +109,7 @@ while True:
     basex -= VEL_AVANZ
 
     # GRAVITA
-    uccello_vely += 1
+    uccello_vely += 0.85
     uccelloy += uccello_vely
     # COMANDI
     for event in pygame.event.get():
@@ -104,9 +118,23 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
     #gestione tubi
-    if tubi[-1].x<60: tubi.append(tubi_classe())
+    if tubi[-1].x<130: tubi.append(tubi_classe())
     for t in tubi:
         t.collissione(uccello,uccellox,uccelloy)
+    
+    if not fra_i_tubi:
+        for t in tubi:
+            if t.fra_i_tubi(uccello,uccellox):
+                fra_i_tubi = True
+                break
+    if fra_i_tubi:
+        fra_i_tubi = False
+        for t in tubi:
+            if t.fra_i_tubi(uccello,uccellox):
+                fra_i_tubi =  True
+                break
+        if not fra_i_tubi:
+                punti+=1
     #colisone con la base
     if (uccelloy > 380):
         hai_perso()
